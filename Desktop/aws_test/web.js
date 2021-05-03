0002ed7ceb02b85ded8efc66c49debc246d55d49
= <=>
2021-05-03 15:04:56 +0900

    test

---
+++ Desktop/aws_test/web.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use('/', (req,res)=>{
+    res.send(`
+        <h3>Hello AWS World</h3>
+    `);
+});
+
+app.listen(port,()=>{console.log("server on")});
